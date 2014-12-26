7902d79509043f090b2c2e677c286ca928f36234
vinayaka <vinayaka@192.168.4.27>
2014-12-26 19:58:17 +0530

    User creation and sign in working examples added.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,26 +1,97 @@
 'use strict';
 
-angular.module('tabApp.controllers', ['tabApp.services']).controller("TableauController", ['$scope', 'TableauService', function ($scope, TableauService) {
+angular.module('tabApp.controllers', ['tabApp.services']).controller("TableauController", ['x2js', '$scope', 'TableauService', function (x2js, $scope, TableauService) {
         $scope.appName = "Tableau Integration";
 
         $scope.init = function () {
-            console.info("In init");
             var conf = {
                 tsRequest: {
                     credentials: {
-                        name: "ashwin.mridul",
-                        password: "ashwin13#",
+                        _name: "ashwin.mridul",
+                        _password: "ashwin13#",
                         site: {
-                            contentUrl: ""
+                            _contentUrl: ""
                         }
                     }
                 }
             };
-            TableauService.doLogin(conf).then(function (response) {
-                console.info(response.data);
+            TableauService.doLogin(x2js.json2xml_str(conf)).then(function (response) {
+                var res = x2js.xml_str2json(response.data);
+                var token = res['tsResponse']['credentials']['_token'];
+                var placeholderDiv = document.getElementById('placeHolder');
+                var url = 'http://uiatab.glassbeam.com:8000/datasources';
+                var options = {
+                    hideHeaders: true,
+                    hideTabs: true,
+                    width: "800px",
+                    height: "700px"
+                };
+                var viz = new tableauSoftware.Viz(placeholderDiv, url, options);
             }, function (response) {
-                console.info(response);
+                console.error(response);
             });
         };
         $scope.init();
+    }]).controller('NewUserController', ['x2js', '$scope', 'TableauService', function (x2js, $scope, TableauService) {
+        $scope.loadPage = function () {
+            var conf = {
+                tsRequest: {
+                    credentials: {
+                        _name: "ashwin.mridul",
+                        _password: "ashwin13#",
+                        site: {
+                            _contentUrl: ""
+                        }
+                    }
+                }
+            };
+            TableauService.doLogin(x2js.json2xml_str(conf)).then(function (response) {
+                var token = x2js.xml_str2json(response.data)['tsResponse']['credentials']['_token'];
+//                conf = {
+//                    tsRequest: {
+//                        user:{
+//                            _name:$scope.username,
+//                            _role:"Viewer",
+//                            _publish:"true",
+//                            _contentAdmin:"false",
+//                            _suppressGettingStarted:"false"
+//                        }
+//                    }
+//                };
+                conf = {
+                    tsRequest: {
+                        site: {
+                            _name: $scope.username,
+                            _contentUrl: $scope.username,
+                            _disableSubscriptions: "false"
+                        }
+                    }
+                };
+//                TableauService.addUser(x2js.json2xml_str(conf), token).then(function (response) {
+                TableauService.addSite(x2js.json2xml_str(conf), token).then(function (response) {
+                    conf = {
+                        tsRequest: {
+                            user: {
+                                _name: $scope.username,
+                                _role: "Viewer",
+                                _publish: "true",
+                                _contentAdmin: "false",
+                                _suppressGettingStarted: "false"
+                            }
+                        }
+                    };
+                    var site_id  =x2js.xml_str2json(response.data)['tsResponse']['site']['_id'];
+                    TableauService.addUser(x2js.json2xml_str(conf), token, site_id).then(function (response) {
+                        console.log(x2js.xml_str2json(response.data));
+                    }, function(response){
+                        console.error(response)
+                    });
+                }, function (response) {
+                    console.error(response);
+                });
+            }, function (response) {
+                console.error(response);
+            });
+
+        };
     }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,13 +1,34 @@
 'use strict';
 
-angular.module('tabApp.services', ['xml']).factory('TableauService', ['x2js', '$http', function(x2js, $http){
+angular.module('tabApp.services', ['xml']).factory('TableauService', ['$http', function($http){
        return {
          doLogin : function(param){
-             console.info(x2js.json2xml(param));
+//             console.info(x2js.xml_str2json('<tsRequest><credentials name="admin-username" password="admin-password" ><site contentUrl="target-site-content-URL" /></credentials></tsRequest>'));
+//             console.info(x2js.json2xml_str(param));
              return $http({
                  url : 'http://uiatab.glassbeam.com:8000/api/2.0/auth/signin',
                  method : 'POST',
-                 data : x2js.json2xml(param)
+                 data : param
+             });
+         }, 
+         addSite : function(param, token){
+             return $http({
+                url : 'http://uiatab.glassbeam.com:8000/api/2.0/sites',
+                method : 'POST',
+                headers : {
+                    "X-Tableau-Auth" : token
+                },
+                data : param
+             });
+         }, 
+         addUser : function(param, token, site_id){
+             return $http({
+                url : 'http://uiatab.glassbeam.com:8000/api/2.0/sites/'+site_id+'/users/',
+                method : 'POST',
+                headers : {
+                    "X-Tableau-Auth" : token
+                },
+                data : param
              });
          }
        };
